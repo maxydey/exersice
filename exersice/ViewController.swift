35eb40e13829d482f2c1bb576409ad40dd18d467
Max Deygin <maxydey@gmail.com>
2018-08-14 10:47:37 +0300

    Initial Commit

---
+++ exersice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exersice
+//
+//  Created by Max Deygin on 8/14/18.
+//  Copyright © 2018 Max Deygin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
